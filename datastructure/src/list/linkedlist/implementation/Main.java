04c245ca4e5879d9302246578665effaab78915f
jegeun Lee <jklee9169@gamil.com>
2022-03-30 09:48:29 +0900

    자료구조 공부하자

---
+++ datastructure/src/list/linkedlist/implementation/Main.java
@@ -0,0 +1,18 @@
+package list.linkedlist.implementation;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		 ArrayList numbers = new ArrayList();
+		 
+		 numbers.addLast(10);
+		 numbers.addLast(20);
+		 numbers.addLast(30);
+		 numbers.addLast(40);
+		 
+		 numbers.add(1, 15);
+		 numbers.addFirst(5);
+	}
+
+}
